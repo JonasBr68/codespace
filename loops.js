6a30d1c4716b73ee3c569688a072fb0fb78b5811
Jonas Brandel <jonas.brandel@gmail.com>
2018-02-08 18:43:44 +0100

    Added for loop

--- loops.js
+++ loops.js
@@ -37,3 +37,10 @@ while(pos < hello.length)
 {
     console.log(hello[pos++]);
 }
+
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+}
