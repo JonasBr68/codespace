26b46337768ea2bc310f32bba9a4de7a020822b5
JonasBr68 <jonas.brandel@gmail.com>
2018-02-20 21:57:07 +0100

    update

---
+++ carga_camioneta.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-alert, no-console, no-undef */
+
+var queue = [123, 342, 23, 211, 23, 435, 44, 332, 224, 543, 7];
+
+function howManyRuns(queueOfWeights) {
+    var loadActual = 0;
+    const maxWeight = 550;
+    var runsNeeded = 0;
+    for (let i = 0; i < queueOfWeights.length; i++) {
+        if (loadActual + queueOfWeights[i] <= maxWeight) {
+            //Podemos cargar ese tambien
+            loadActual += queueOfWeights[i];
+        }
+        else {
+            //No podemos cargar ese
+            runsNeeded++; //Necesita otra vuelta
+            loadActual = queueOfWeights[i]; //Empezamos una nuva carga con ese
+        }
+    }
+    return runsNeeded;
+}
+
+console.log(howManyRuns(queue));
+
+
+function sortWeights(weights)
+{
+    var arrSorted = weights.sort((a, b) => { return b - a;});
+    return arrSorted;
+}
+
+function optimizeRuns(queue)
+{
+    var heavyFirst = sortWeights(queue);
+
+    var comboMap = [];
+
+    var nextCombo = getCombo(heavyFirst, comboMap);
+    
+
+}
+
+function getCombo(heavyFirst, comboMap, currentCombo)
+{
+    for (let pos = 0; pos < heavyFirst.length; pos++) {
+        if(comboMap[pos] == undefined)
+        {
+            //Not used, find the biggest next pairing that fits
+            var currentComboSize = 0;
+            for(let s=heavyFirst.length - 1;s>pos;s--)
+            {
+                if(comboMap[s] == undefined)
+                {
+                    //Unused, does it fit
+                    unfinished...
+                    if(heavyFirst[s] + heavyFirst[pos] <= 550)
+                    {
+                        currentComboSize = 
+                        //usalo
+                        comboMap[pos] = currentCombo;
+                        comboMap[s] = currentCombo;
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+console.log(sortWeights(queue));
+
+
--- for_loops.js
+++ for_loops.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-alert, no-console, no-undef */
 
-for (var counter = 0; counter < 15; counter++) {
-    //hacer algo
-    console.log(counter);
-}
+// for (var counter = 0; counter < 15; counter++) {
+//     //hacer algo
+//     console.log(counter);
+// }
 
-var pos = 0;
-for(;pos < 10;)
-{
-    if(pos > 10)
-        break;
-    pos++;
-}
+// var pos = 0;
+// for(;pos < 10;)
+// {
+//     if(pos > 10)
+//         break;
+//     pos++;
+// }
 
-console.log(pos);
+// console.log(pos);
 
-//Ejercicio 1
-//Con un bucle for imprime en la consola todos números impares hasta 31 incluido
-for(var i=1;i<=31;i++)
-{
-    if(i % 2 != 0)
-        console.log(i);
-}
+// //Ejercicio 1
+// //Con un bucle for imprime en la consola todos números impares hasta 31 incluido
+// for(var i=1;i<=31;i++)
+// {
+//     if(i % 2 != 0)
+//         console.log(i);
+// }
 
 //Ejercicio 2
 //Tienes un Array con nombres como esta:
@@ -29,8 +29,49 @@ var nombres = ["Mary", "Ana", "Belinda", "Maria"];
 //Con un bucle for encuentra todos los nombres que empieza con 'M'
 //y imprime las en la consola.
 
-for (let index = 0; index < nombres.length; index++) {
-    const nombre = nombres[index];
-    if(nombre[0] == "M")
-        console.log(nombre);
+// for (let index = 0; index < nombres.length; index++) {
+//     const nombre = nombres[index];
+//     if(nombre[0] == "M")
+//         console.log(nombre);
+// }
+
+
+function reverse(arrIn) {
+    var arrReversed = [];
+    for (var i = arrIn.length - 1; i >= 0; i--) {
+        const element = arrIn[i];
+        arrReversed.push(element);
+    }
+    return arrReversed;
 }
+
+console.log(reverse(nombres));
+
+
+function validateReverse(arrOriginal, arrInverted) {
+    if (arrOriginal.length != arrInverted.length)
+        return false;
+
+    var posStartOriginal = 0;
+    var posStartInverted = arrInverted.length - 1;
+
+    for (let desvio = 0; desvio < arrOriginal.length; desvio++) {
+        var posActualOriginal = posStartOriginal + desvio;
+        var posActualInverted = posStartInverted - desvio;
+
+        var elementoOriginal = arrOriginal[posActualOriginal];
+
+        var elementoInverted = arrInverted[posActualInverted];
+        if (elementoOriginal != elementoInverted) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(validateReverse(nombres, reverse(nombres)));
+console.log(validateReverse(nombres, nombres));
+
+
+
